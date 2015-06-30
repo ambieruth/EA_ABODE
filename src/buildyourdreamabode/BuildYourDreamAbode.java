3a01e7780622f6e534c4e6a0f72ee50360cb8370
ambiesnell <ambiesnell@10.0.0.10>
2015-06-29 23:29:20 -0600

    6/29

---
+++ src/buildyourdreamabode/BuildYourDreamAbode.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buildyourdreamabode;
+
+import byui.cit260.buildYourDreamAbode.model.Player;
+
+/**
+ *
+ * @author ambiesnell
+ */
+public class BuildYourDreamAbode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne= new Player();
+        
+        playerOne.setName("Joe Smith");
+        playerOne.setBestTime(12.00);
+        
+        String playerInfo=playerOne.toString();
+        System.out.println(playerInfo);
+        
+    }
+    
+}
