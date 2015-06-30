19f4afbc4c2b172b52da8faf4acbb4a1c878eae7
ambiesnell <ambiesnell@10.0.0.10>
2015-06-29 23:56:16 -0600

    6/29

--- src/buildyourdreamabode/BuildYourDreamAbode.java
+++ src/buildyourdreamabode/BuildYourDreamAbode.java
@@ -5,6 +5,7 @@
  */
 package buildyourdreamabode;
 
+import byui.cit260.buildYourDreamAbode.model.House;
 import byui.cit260.buildYourDreamAbode.model.Player;
 
 /**
@@ -25,6 +26,14 @@ public class BuildYourDreamAbode {
         String playerInfo=playerOne.toString();
         System.out.println(playerInfo);
         
+        House houseOne = new House ();
+        
+        houseOne.setDescription("Your new home is now built. Now it is time to furnish it.");
+        houseOne.setNumberOfBedrooms(5);
+        houseOne.setNumberOfBathrooms(5.5);
+        
+        String houseInfo=houseOne.toString();
+        System.out.println(houseInfo);
     }
     
 }
